cccef351d388048218fbcefa086307ab1fd8b115
Novav20 <108189981+Novav20@users.noreply.github.com>
2022-07-15 07:01:05 -0500

    Add files via upload

---
+++ Hospitalization/HaHome.App/HaHome.App.Frontend/Pages/FormPatient/FormPatient.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HaHome.App.Frontend
+{
+    public class FormPatientModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

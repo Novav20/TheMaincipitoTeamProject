7527ccc58aa5b0677115d5f32b97b2e869441bf7
camorabo <80613684+camorabo@users.noreply.github.com>
2022-07-17 10:00:39 -0500

    Creación de la carpeta Consola

---
+++ HospiEnCasa.App.Consola/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
+
+namespace HospiEnCasa.App.Consola
+{
+
+    class Program
+    {
+        private static IRepositorioPaciente _repoPaciente = new RepositorioPaciente(new Persistencia.AppContext());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        private static void AddPaciente()
+        {
+            var paciente = new Paciente
+            {
+                Nombre = "Pepito",
+                Apellidos = "Perex Prieto",
+                Telefono = "300000",
+                Genero = Genero.masculino,
+                Direccion = "Calle 8",
+                longitud = 45.5F,
+                latitud = 5.8F,
+                Ciudad = "Santa Marta",
+                FechaNacimiento = new DateTime(1972, 08, 22),
+
+            };
+            _repoPaciente.AddPaciente(paciente);
+        }
+    }
+}

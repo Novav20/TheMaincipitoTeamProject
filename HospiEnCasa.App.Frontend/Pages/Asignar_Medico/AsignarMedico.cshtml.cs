39995d1ae84166cc80579aa5d2842e8497b04f1f
LoondoN <89754189+LoondoN@users.noreply.github.com>
2022-07-23 09:01:47 -0500

    Add files via upload

---
+++ HospiEnCasa.App.Frontend/Pages/Asignar_Medico/AsignarMedico.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Persistencia;
+using HospiEnCasa.App.Dominio;
+
+
+namespace MyApp.Namespace
+{
+    public class AsignarMedicoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
